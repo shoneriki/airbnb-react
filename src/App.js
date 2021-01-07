6098d1bbaff649c725c3ef6e542553d279a9ff94
shoneriki <ikirenohs@gmail.com>
2021-01-07 20:35:27 +0900

    flat card styling

--- src/App.js
+++ src/App.js
@@ -14,10 +14,19 @@ class App extends Component {
       "lng": 2.343543
     };
     return (
-      <div>
-        <Flat flat={flat} />
-        <Flat flat={flat} />
+      <div className="app">
+      <div className="main">
+        <div className="search">
+        </div>
+        <div className="flats">
+          <Flat flat={flat}/>
+          <Flat flat={flat}/>
+          <Flat flat={flat}/>
+        </div>
       </div>
+      <div className="map">
+      </div>
+    </div>
     );
   }
 }
